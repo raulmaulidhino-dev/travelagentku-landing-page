c35024ed7e704bd332ff7fc7049d671662a591ab
Raul Maulidhino <raulahmadm314@gmail.com>
2024-10-05 21:14:26 +0700

    style: add scroll animations with ScrollRevealjs

---
+++ js/scroll-animations.js
@@ -0,0 +1,45 @@
+// Reveal Animations for Header
+ScrollReveal().reveal('header > .logo', {scale:0, duration: 1000});
+ScrollReveal().reveal('.desktop-nav', {scale:0, duration: 1000});
+ScrollReveal().reveal('.desktop-nav td > a', {delay: 1000, scale:0, duration: 1000});
+
+// Reveal Animations for Hero
+ScrollReveal().reveal('.heading > h1', { delay: 100, scale: 0.5, origin: 'left', duration:'1000' });
+ScrollReveal().reveal('.heading > h3', { delay: 500, distance:'100px', origin: 'bottom', duration:'1000' });
+
+// Reveal Animations for Content Image (Banners)
+ScrollReveal().reveal('.content-image', {delay: 500, scale: 0.75, duration: 1000});
+
+// Reveal Animations for Review section
+ScrollReveal().reveal('.review > q', {distance:'80px', origin:'top', duration: 1000});
+ScrollReveal().reveal('.reviewer-bio', {delay: 500, distance:'80px', origin:'top', duration: 1000});
+ScrollReveal().reveal('.reviewer-info > img', {delay: 500, scale:0});
+
+// Reveal Animations for Benefits section
+ScrollReveal().reveal('.benefits > p', {delay: 500, distance: '100px', origin:'left', duration: 1000});
+ScrollReveal().reveal('.benefits-list > div > div', {delay: 500, scale: 0.25, duration: 1000});
+ScrollReveal().reveal('.benefits-list > div > p', {delay: 1000, distance: '100px', origin:'left', duration: 1000});
+
+// Reveal Animations for Contact Us Form section
+ScrollReveal().reveal('#contact_us', {delay: 500, distance: '150px', origin: 'right', duration: 500});
+ScrollReveal().reveal('.contact-us-title > h3', {delay: 1000, scale: 0, origin: 'left', duration: 1000});
+ScrollReveal().reveal('.contact-us-title > p', {delay: 1500, distance:'100px', origin:'top', duration: 1000});
+ScrollReveal().reveal('#contact_us_form > section > label', {delay: 1000, scale: 0, duration: 1000});
+ScrollReveal().reveal('#contact_us_form > section > input', {delay: 1750, distance:'50px', origin:'top', duration: 1000});
+ScrollReveal().reveal('#contact_us_form > section > select', {delay: 1750, distance:'50px', origin:'top', duration: 1000});
+ScrollReveal().reveal('#send_button', {delay: 1750, scale: 0, duration: 1000});
+
+// Reveal Animations for Our Package section
+ScrollReveal().reveal('#our_package > h2', {scale: 0, duration: 1000});
+ScrollReveal().reveal('.package > div > img', {scale: 0, duration: 1000});
+ScrollReveal().reveal('.package > section > h4', {distance:'100px', origin: 'left', duration: 1000});
+ScrollReveal().reveal('.package > section > p', {delay: 1000, distance:'100px', origin: 'top', duration: 1000});
+
+// Reveal Animations for Call Us section
+ScrollReveal().reveal('#call_us > div > h2', {scale:0, duration: 1000});
+ScrollReveal().reveal('#call_us > div > p', {delay: 1000, distance:'100px', origin: 'top', duration: 1000});
+ScrollReveal().reveal('.cta', {delay: 1000, scale:0, duration: 1000});
+
+// Reveal Animations for Footer
+ScrollReveal().reveal('footer > .logo', {scale: 0, duration: 1000});
+ScrollReveal().reveal('footer > :not(.logo)', {delay: 1000, distance: '75px', origin: 'top', duration: 1000});
